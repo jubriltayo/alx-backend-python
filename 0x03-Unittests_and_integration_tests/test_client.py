ef4d5853bcf40d84b819edd5019cc9388d0f25b8
Tayo Jubril <jubriltayo@gmail.com>
2023-10-31 00:29:22 +0100

    task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -25,7 +25,7 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_fn.assert_called_once_with(f"https://api.github.com/orgs/{org}")
 
     def test_public_repos_url(self) -> None:
-        """test public repos to give correct result"""
+        """test public repos url to give correct result"""
         with patch("client.GithubOrgClient.org",
                    new_callable=PropertyMock) as mock:
             mock.return_value = {
@@ -35,3 +35,54 @@ class TestGithubOrgClient(unittest.TestCase):
                 client.GithubOrgClient("google")._public_repos_url,
                 "https://api.github.com/users/google/repos"
             )
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """test public repos to give correct result"""
+        test_payload = {
+            "repos_url": "https://api.github.com/users/google/repos",
+            "repos": [
+                {
+                    "id": 460600860,
+                    "name": ".allstar",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/.allstar",
+                    "created_at": "2022-02-17T20:40:32Z",
+                    "updated_at": "2023-04-03T17:58:33Z",
+                    "has_issues": True,
+                    "forks": 3,
+                    "default_branch": "main"
+                },
+                {
+                    "id": 91820777,
+                    "name": "abpackage",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/abpackage",
+                    "created_at": "2017-05-19T15:38:35Z",
+                    "updated_at": "2023-09-29T05:40:56Z",
+                    "has_issues": False,
+                    "forks": 18,
+                    "default_branch": "master"
+                }
+            ]
+        }
+        mock_get_json.return_value = test_payload["repos"]
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_public_repos:
+            mock_public_repos.return_value = test_payload["repos_url"]
+            self.assertEqual(client.GithubOrgClient("google").public_repos(),
+                            [".allstar",
+                              "abpackage"
+                            ])
+            mock_public_repos.assert_called_once()
+        mock_get_json.assert_called_once()
