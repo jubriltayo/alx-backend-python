0c68ba585ca4c89bdff4bb2b2728785b2aa5ee5d
Tayo Jubril <jubriltayo@gmail.com>
2023-10-30 13:13:42 +0100

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,11 +4,12 @@
 from utils import access_nested_map
 import unittest
 from parameterized import parameterized
-from typing import Mapping, Tuple, Union
+from typing import Mapping, Tuple, Union, Type
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Class for Unit Tests using parameterization"""
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -18,3 +19,14 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: Union[Mapping, int]) -> None:
         """Unit Test for different test cases"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Tuple[str],
+                                         expected: Type[Exception]) -> None:
+        """Unit test for different test cases"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
