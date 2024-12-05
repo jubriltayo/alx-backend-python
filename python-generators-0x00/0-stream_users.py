d511a8fee104455d5f7ab11cbb4d0e5619377023
Tayo <jubriltayo@gmail.com>
2024-12-05 21:30:29 +0100

    generator function - NO need to use fetchall() at once

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,21 @@
+import pymysql
+
+
+def stream_users():
+    """
+    Generator function to fetch rows one by one from the user_data table
+    """
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user_data")
+    
+    for row in cursor:
+        yield row
+    
+    cursor.close()
+    connection.close()
