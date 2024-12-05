c1fe1aef692ee8f3be14f15cf506bd4d2033b3c5
Tayo <jubriltayo@gmail.com>
2024-12-05 23:50:20 +0100

    batch processing large data

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,39 @@
+import pymysql
+import sys
+
+
+def stream_users_in_batches(batch_size):
+    """
+    Fetch rows from the user_data in batches using a generator
+    """
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT user_id, name, email, age FROM user_data")
+
+    batch = []
+    for user in cursor:
+        batch.append(user)
+        if len(batch) >= batch_size:
+            yield batch
+            batch = [] 
+
+    if batch:
+        yield batch # last batch
+    
+    cursor.close()
+    connection.close()
+
+def batch_processing(batch_size):
+    """ Process each batch to filter users over the age of 25 """
+    try:
+        for batch in stream_users_in_batches(batch_size):
+            filtered_batch = [user for user in batch if user[3] > 25] # filter users with age over 25
+            for user in filtered_batch:
+                print(user)
+    except (BrokenPipeError, OSError):
+        sys.stderr.close()
