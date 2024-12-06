2fa40d62517a93880682567c8f639cc8545ea6bd
Tayo <jubriltayo@gmail.com>
2024-12-06 12:43:18 +0100

    log database queries before execution using decorator

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,30 @@
+import sqlite3
+import functools
+
+# decorator to log SQL queries
+def log_queries(func):
+    @functools.wraps(func) # Ensure original function name instead of wrapper. fetch_all_users.__name__ == fetch_all_users
+    def wrapper(*args, **kwargs):
+        # if kwargs get 'query', if args get the first arg
+        query = kwargs.get('query', args[0] if args else None)
+        if query:
+            print(f"Executing SQL Query: {query}")
+        else:
+            print("No query found to log.")
+        return func(*args, **kwargs) # call the original function
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+
+for user in users:
+    print(user)
---
+++ python-decorators-0x01/setup_database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+# Set up SQLite database and create table
+def setup_database():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    # Create table if it doesn't exist
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        name TEXT,
+        email TEXT,
+        age INTEGER
+    )
+    ''')
+
+    # Insert sample data if the table is empty
+    cursor.execute('SELECT COUNT(*) FROM users')
+    if cursor.fetchone()[0] == 0:  # No data
+        cursor.executemany('INSERT INTO users (user_id, name, email, age) VALUES (?, ?, ?, ?)', [
+            ('1', 'Miss Arlene Herzog', 'Ada33@gmail.com', 103),
+            ('2', 'John Doe', 'john.doe@example.com', 35),
+            ('3', 'Jane Smith', 'jane.smith@example.com', 28)
+        ])
+    conn.commit()
+    conn.close()
+
+# Call setup function
+setup_database()
